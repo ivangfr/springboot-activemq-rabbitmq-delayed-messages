81c0490e33c92f237af4114777255d5a23fe6317
Ivan Franchin <ivan.franchin@takeaway.com>
2024-11-13 19:36:31 +0000

    Replace Jib with Paketo Buildpacks

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-./mvnw clean compile jib:dockerBuild --projects delayed-message-producer-consumer
+DOCKER_IMAGE_PREFIX="ivanfranchin"
+APP_NAME="delayed-message-producer-consumer"
+APP_VERSION="1.0.0"
+DOCKER_IMAGE_NAME="${DOCKER_IMAGE_PREFIX}/${APP_NAME}:${APP_VERSION}"
+SKIP_TESTS="true"
+
+./mvnw clean spring-boot:build-image --projects "$APP_NAME" -DskipTests="$SKIP_TESTS" -Dspring-boot.build-image.imageName="$DOCKER_IMAGE_NAME"
